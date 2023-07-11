1f628b54532ab20c1fb69fb969db1503c0f34b8a
Crazy_Quackers <omerkohavi2004@gmail.com>
2023-07-11 10:03:34 +0300

    router stuff

--- authorization/routes/users.js
+++ authorization/routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const database = {
+    isAdmin: (id) => true,
+    isOrganization: (id) => true,
+    isTeacher: (id) => true,
+    isStudent: (id) => true,
+    isValidToken: (token) => true,
+};
+
+/* is User an Admin */
+router.get('/users/:id/isAdmin', function(req, res, next) {
+  res.send(database.isAdmin(req.params.id));
+});
+
+/* is User an Organization */
+router.get('/users/:id/isOrganization', function(req, res, next) {
+    res.send(database.isOrganization(req.params.id));
+});
+
+/* is User a Student */
+router.get('/users/:id/isStudent', function(req, res, next) {
+    res.send(database.isStudent(req.params.id));
+});
+  
+/* is User a Teacher */
+router.get('/users/:id/isTeacher', function(req, res, next) {
+    res.send(database.isTeacher(req.params.id));
+});
+
+/* is Token Valid */
+router.get('/isValidToken/:token', function(req, res, next) {
+    res.send(database.isValidToken(req.params.token));
 });
 
 module.exports = router;
