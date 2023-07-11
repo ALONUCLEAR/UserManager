38fa111c495fdf4f9508bd96c9589f81e9de559b
ALONUCLEAR <hadas716@gmail.com>
2023-07-11 16:17:09 +0300

    start getway

--- gateway/routes/index.js
+++ gateway/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/user', function(req, res, next) {
+    try {
+        fetch('localhost:1000/')
+        .then(handleErrors)
+        server.getUser(req.params.id);
+    } catch ({ message }) {
+        res.status(500).end(message);
+    }
+});      
 
 module.exports = router;
