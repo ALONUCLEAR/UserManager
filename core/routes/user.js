806872e3b2371f1832ad15ae952f4d2968052cd2
ALONUCLEAR <hadas716@gmail.com>
2023-07-11 14:14:53 +0300

    status to response

--- core/routes/user.js
+++ core/routes/user.js
@@ -6,36 +6,36 @@ const {getUserByID, createUser, changeRoleByUserId, changePasswordByUserId, dele
 router.get('/', async (req, res) => {
     try {
       const user = await getUserByID(req.body.user);
-      res.send(user);
+      res.status(200).send(user);
     } catch (err) {
-      res.send("error when getting user: " + err);
+      res.status(500).send("error when getting user: " + err);
     }
   });
 
 router.post('/create', async (req, res) => {
     try {
       const user = await createUser(req.body.user);
-      res.send(user);
+      res.status(200).send(user);
     } catch (err) {
-      res.send("error when create user: " + err);
+      res.status(500).send("error when create user: " + err);
     }
   });
   
 router.post('/update', async (req, res) => {
     try {
       const user = await createUser(req.body.user);
-      res.send(user);
+      res.status(200).send(user);
     } catch (err) {
-      res.send("error when update user: " + err);
+      res.status(500).send("error when update user: " + err);
     }
   });
 
 router.post('/setRole/:id', async (req, res) => {
     try {
       await changeRoleByUserId(req.params.id, req.body.role);
-      res.send("Role changed successfully ");
+      res.status(200).send("Role changed successfully ");
     } catch (err) {
-      res.send("error when update user: " + err);
+      res.status(500).send("error when update user: " + err);
     }
   });
 
@@ -43,9 +43,9 @@ router.post('/setRole/:id', async (req, res) => {
 router.post('/setPassword/:id', async (req, res) => {
     try {
       await changePasswordByUserId(req.params.id, req.body.password);
-      res.send("Password changed successfully ");
+      res.status(200).send("Password changed successfully ");
     } catch (err) {
-      res.send("error when change password: " + err);
+      res.status(500).send("error when change password: " + err);
     }
   });
 
@@ -53,9 +53,9 @@ router.post('/setPassword/:id', async (req, res) => {
 router.delete('/delete/:id',async (req, res) => {
     try {
       await deleteUserByID(req.params.id);
-      res.send("User deleted successfully ");
+      res.status(200).send("User deleted successfully ");
     } catch (err) {
-      res.send("Error when deleting password: " + err);
+      res.status(500).send("Error when deleting password: " + err);
     }
   });
 
